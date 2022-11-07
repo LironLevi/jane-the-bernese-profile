361cf4f9cfffb9c0373dcef9ad62c78283e503e8
LironLevi <lir2143@gmail.com>
2022-11-07 22:51:53 +0200

    ex1 jane profile

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-contactButton = document.querySelector("#contact button");
 
-contactButton.addEventListener("click", function contactClickhandler(el) {
-  alert("Not implemented");
-});
+
+hidemode = false;
+document.getElementById("factsB").onclick = () => {
+      hidemode = !hidemode;
+      document.getElementById("facts").hidden = hidemode;
+      if (hidemode){
+        document.getElementById("factsB").innerHTML = "show facts about my type";
+      }
+      else{
+        document.getElementById("factsB").innerHTML = "hide facts about my type";
+      }
+  }
+
+
+changecolor = true;
+document.getElementById("ChangeColorB").onclick = () => {
+      changecolor = !changecolor;
+      if (changecolor){
+        document.body.style.backgroundColor = "#FFDAB9";
+        document.body.style.color = "black";
+      }
+      else{
+        document.body.style.backgroundColor = "#E9967A"; 
+        document.body.style.color = "white";
+      }
+  }
